4e8b7cb8116664d059849dde37ef6576765d873d
carlajayne <67230580+carlajayne@users.noreply.github.com>
2021-03-14 03:27:30 -0400

    preloader push - good

--- js/index.js
+++ js/index.js
@@ -3,4 +3,7 @@ console.log('Your index.js file is loaded correctly!');
 
 
 /*Preloader*/
-$(.preloader).fadeOut('slow');
+function fade() {
+    $('.preloader').fadeOut("slow");
+    }
+    setTimeout(fade, 3000);
