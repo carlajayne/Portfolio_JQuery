12005beeb675bba3235dc195d285610d666870f3
carlajayne <67230580+carlajayne@users.noreply.github.com>
2021-03-14 03:58:33 -0400

    last push
    smooth scroll - css
    preloader - java

--- js/index.js
+++ js/index.js
@@ -7,3 +7,18 @@ function fade() {
     $('.preloader').fadeOut("slow");
     }
     setTimeout(fade, 3000);
+
+/*Smooth Scroll - was not able to properly execute
+$(document).ready(function(){
+    $("#backtop").on('click', function(event) {
+        if (this.hash !=="") {
+            event.preventDefault();
+            var hash= this.hash;
+            $('html, body').animate({
+                scrollTop:$(hash).offset().top
+            },800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});*/
